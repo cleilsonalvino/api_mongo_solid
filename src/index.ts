a97c91bb94f9b78a1bdc4e24625cfaf5202fdb6b
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2026-03-01 22:14:49 -0300

    chore: connect mongodb in server

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,34 @@
 import express from "express";
-import {config} from "dotenv";
+import { config } from "dotenv";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import { MongoClient } from "./database/mongo";
 
 config();
 
-const app = express();
+const main = async () => {
+  const app = express();
 
-const PORT = process.env.PORT || 3000;
+  await MongoClient.connect()
 
-app.get("/users", async (req, res) => {
+  app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
 
-  const mongoGetUsersRepository = new MongoGetUsersRepository()
-  const getUsersController = new GetUsersController(mongoGetUsersRepository)
+    const { code, body } = await getUsersController.handle();
 
-  const {code, body} = await getUsersController.handle()
+    res.send(body).status(code);
+  });
 
-  res.send(body).status(code)
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+
+main().catch((error)=>{
+    console.log('Error in the server', error)
+})
